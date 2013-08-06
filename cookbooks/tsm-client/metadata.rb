b1f4759ae7f57e25e920774888403d922d50f200
SergioVicenteRuiz <sergiovicente2008@hotmail.es>
2013-08-06 10:17:49 +0200

    tsm-client cookbook edited

--- cookbooks/tsm-client/metadata.rb
+++ cookbooks/tsm-client/metadata.rb
@@ -1,7 +1,10 @@
 name             'tsm-client'
 maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer_email 'sergiovicente@es.ibm.com'
 license          'All rights reserved'
 description      'Installs/Configures tsm-client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends          'windows'
+depends          'java-modified'
