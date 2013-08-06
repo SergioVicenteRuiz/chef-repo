33c0f3892aeb8731f1aa8f006493b80566095b79
SergioVicenteRuiz <sergiovicente2008@hotmail.es>
2013-08-06 18:09:02 +0200

    symantec-modified cookbook modified

--- cookbooks/symantec-modified/recipes/undo.rb
+++ cookbooks/symantec-modified/recipes/undo.rb
@@ -21,12 +21,11 @@
 
 execute "Uninstall Symantec Endpoint Protection" do
   command "msiexec /qn /x #{node['symantec']['appid']} /l*v %temp%/uninstall_sep.log"
-  notifies :request, 'windows_reboot[60]'
 end
 
 windows_reboot 60 do
   reason 'Reboot required for Symantec Endpoint uninstall'
-  action :nothing
+  action :request
 end
 
 ruby_block "remove symantec-modified::undo from run list" do
