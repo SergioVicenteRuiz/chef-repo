d33447a00db302692e99c354648103a3a277540b
SergioVicenteRuiz <sergiovicente2008@hotmail.es>
2013-08-06 13:54:21 +0200

    vcredist cookbook

--- cookbooks/vcredist/attributes/default.rb
+++ cookbooks/vcredist/attributes/default.rb
@@ -17,14 +17,14 @@
 # limitations under the License.
 #
 
-default['vcredist_x86']['extract_source'] = "http://yum.#{node['domain']}:8080/vcredist/vcredist_x86.exe"
-default['vcredist_x86']['appid'] = '{F0C3E5D1-1ADE-321E-8167-68EF0DE699A5}'
+default['vcredist_x86']['install_source'] = "http://yum.#{node['domain']}:8080/vcredist/vcredist_x86.exe"
 default['vcredist_x86']['install_options'] = '/q /c:"msiexec /i vcredist.msi /qn /l*v"'
+default['vcredist_x86']['appid'] = '{F0C3E5D1-1ADE-321E-8167-68EF0DE699A5}'
 
 if kernel.machine =~ /x86_64/
-  default['vcredist_x64']['extract_source'] = "http://yum.#{node['domain']}:8080/vcredist/vcredist_x64.exe"
-  default['vcredist_x64']['appid'] = '{1D8E6291-B0D5-35EC-8441-6616F567A0F7}'
+  default['vcredist_x64']['install_source'] = "http://yum.#{node['domain']}:8080/vcredist/vcredist_x64.exe"
   default['vcredist_x64']['install_options'] = '/q /c:"msiexec /i vcredist.msi /qn /l*v"'
+  default['vcredist_x64']['appid'] = '{1D8E6291-B0D5-35EC-8441-6616F567A0F7}'
 end
 
 
--- cookbooks/vcredist/recipes/default.rb
+++ cookbooks/vcredist/recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+windows_package "Microsoft Visual C++ 2010  x86 Redistributable - 10.0.40219" do
+  source node['vcredist_x86']['install_source']
+  options node['vcredist_x86']['install_options']
+  installer_type :custom
+  action :install
+end
+
+if kernel.machine =~ /x86_64/
+  windows_package "Microsoft Visual C++ 2010  x64 Redistributable - 10.0.40219" do
+    source node['vcredist_x64']['install_source']
+    options node['vcredist_x64']['install_options']
+    installer_type :custom
+    action :install
+  end
+end
