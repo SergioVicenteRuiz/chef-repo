bdc949a7efedae41792910bc788e7a014ce7e79c
SergioVicenteRuiz <sergiovicente2008@hotmail.es>
2013-08-07 13:12:02 +0200

    tsm-client cookbook

--- cookbooks/tsm-client/attributes/default.rb
+++ cookbooks/tsm-client/attributes/default.rb
@@ -18,15 +18,16 @@
 #
 
 default['tsm']['extract_path'] = "#{Chef::Config[:file_cache_path]}/tsm_images/TSM_BA_Client"
-default['tsm']['dsm_opt_extract_path'] = "#{Chef::Config[:file_cache_path]}/tsm_images/TSM_BA_Client/baclient/Program Files/Tivoli/TSM/config"
 default['tsm']['install_dir'] = "C:/Tivoli/TSM"
 
 if kernel.machine =~ /x86_64/
   default['tsm']['extract_source'] = "http://yum.#{node['domain']}:8080/tsm/6.4.0.0-TIV-TSMBAC-WinX64.exe"
+  default['tsm']['dsm_opt_extract_path'] = "#{Chef::Config[:file_cache_path]}/tsm_images/TSM_BA_Client/baclient/Program Files 64/Tivoli/TSM/config"
   default['tsm']['install_options'] = 'RebootYesNo="No" REBOOT="Suppress" ALLUSERS=1 INSTALLDIR="C:/Tivoli/TSM" ADDLOCAL="BackupArchiveGUI,BackupArchiveWeb,Api64Runtime" TRANSFORMS=1033.mst /qn /l*v "%temp/install_tsm_log.txt"'
   default['tsm']['appid'] = '{F5C1AC15-CB26-44D5-80BD-986463F15B09}'
 else
   default['tsm']['extract_source'] = "http://yum.#{node['domain']}:8080/tsm/6.4.0.0-TIV-TSMBAC-WinX32.exe"
+  default['tsm']['dsm_opt_extract_path'] = "#{Chef::Config[:file_cache_path]}/tsm_images/TSM_BA_Client/baclient/Program Files/Tivoli/TSM/config"
   default['tsm']['install_options'] = 'RebootYesNo="No" REBOOT="Suppress" ALLUSERS=1 INSTALLDIR="C:/Tivoli/TSM" ADDLOCAL="BackupArchiveGUI,BackupArchiveWeb,ApiRuntime" TRANSFORMS=1033.mst /qn /l*v "%temp%/install_tsm_log.txt"'
   default['tsm']['appid'] = '{C2C4809D-6809-43B6-A544-1DE0BA0E40B8}'
 end
