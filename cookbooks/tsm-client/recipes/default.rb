7dfedb91d3159482783ed74f605b8a7d2842f407
SergioVicenteRuiz <sergiovicente2008@hotmail.es>
2013-08-08 13:20:33 +0200

    tsm-client cookbook

--- cookbooks/tsm-client/recipes/default.rb
+++ cookbooks/tsm-client/recipes/default.rb
@@ -39,14 +39,14 @@ windows_package "IBM Tivoli Storage Manager Client" do
   action :install
 end
 
-service node['tsm']['service'] do
-  action :nothing
-end
+#service node['tsm']['service'] do
+#  action :nothing
+#end
 
 execute 'tsm_service_create' do
   command %Q(#{node['tsm']['install_dir']}/dsmcutil.exe install scheduler /name:"#{node['tsm']['service']}" /node:#{node['hostname']} /password:#{node['tsm']['password']} /validate:yes /clientdir:#{install_dir} /optfile:#{install_dir}\\dsm.opt /autostart:yes)
   not_if {Win32::Service.exists?(node['tsm']['service'])}
-  notifies :start, "service[#{node['tsm']['service']}]", :immediately 
+#  notifies :start, "service[#{node['tsm']['service']}]", :immediately 
 end
 
-
+include_recipe "tsm-client::dsm-opt"
