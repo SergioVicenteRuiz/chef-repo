def83aaf7278f585692717e6c5b26b36fc30b07a
root <root@oc4151137872.ibm.com>
2013-08-12 17:05:57 +0200

    isec cookbook created

--- cookbooks/isec/metadata.rb
+++ cookbooks/isec/metadata.rb
@@ -1,11 +1,9 @@
-name             'tsm-client'
+name             'isec'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'sergiovicente@es.ibm.com'
 license          'All rights reserved'
-description      'Installs/Configures tsm-client'
+description      'Installs/Configures isec'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends          'windows'
-depends          'java'
-depends          'vcredist'
