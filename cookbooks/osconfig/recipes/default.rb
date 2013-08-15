cbf305caa74f0641dccb7ecdea977f145d84f068
SergioVicenteRuiz <sergiovicente2008@hotmail.es>
2013-08-15 17:51:04 +0200

    osconfig modified

--- cookbooks/osconfig/recipes/default.rb
+++ cookbooks/osconfig/recipes/default.rb
@@ -9,8 +9,15 @@
 
 
 hostsfile_entry '192.168.122.92' do
-  hostname 'oc0644314035.ibm.com'
-  aliases ['yum.ibm.com','chefserver.ibm.com']
+  hostname 'oc0644314035'
+  aliases ['yum.ibm.com','oc0644314035.ibm.com']
+end
+
+search(:node,"chef_environment:_default").each do |default_node|
+  hostsfile_entry default_node['ipaddress'] do
+    hostname default_node['hostname']
+    aliases default_node['fqdn']
+  end
 end
 
 include_recipe "osconfig::#{node['osconfig']['install_flavor']}"
