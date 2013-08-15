41ef5f141fe6de71e504fcba1083c159a5d0c68c
SergioVicenteRuiz <sergiovicente2008@hotmail.es>
2013-08-15 15:37:21 +0200

    osconfig modified

--- cookbooks/osconfig/recipes/windows.rb
+++ cookbooks/osconfig/recipes/windows.rb
@@ -8,6 +8,10 @@
 #
 
 
-template node['osconfig']['hosts'] do
+template "C:\\Windows\\System32\\Drivers\\etc\\hosts" do
     source "hosts.windows.erb"
 end
+
+#cookbook_file node['osconfig']['hosts'] do
+#  source "hosts"
+#end
