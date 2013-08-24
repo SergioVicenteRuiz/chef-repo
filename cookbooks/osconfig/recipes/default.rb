bf9c76e3b6f46f8381e93096a65a01a0436be972
SergioVicenteRuiz <sergiovicente2008@hotmail.es>
2013-08-24 16:40:46 +0200

    osconfig modified

--- cookbooks/osconfig/recipes/default.rb
+++ cookbooks/osconfig/recipes/default.rb
@@ -9,20 +9,20 @@
 hostsfile_entry '192.168.122.1' do
   hostname 'oc4151137872.ibm.com'
   aliases ['oc4151137872']
-  action :create_if_missing
+#  action :create_if_missing
 end
 
 hostsfile_entry '192.168.122.92' do
   hostname 'oc0644314035.ibm.com'
   aliases ['yum.ibm.com','oc0644314035']
-  action :create_if_missing
+#  action :create_if_missing
 end
 
 search(:node,"chef_environment:_default").each do |default_node|
   hostsfile_entry default_node['ipaddress'] do
     hostname default_node['fqdn']
     aliases [default_node['hostname']]
-    action :create_if_missing
+#    action :create_if_missing
   end
 end
 
